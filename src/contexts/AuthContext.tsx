cc5c7f03a49547217198abd2989794c3c59eb946
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-17 17:28:20 -0400

    Add AuthContext type and AuthContext provider

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,13 @@
+import { createContext } from 'react';
+import { AuthContextType } from '../@types/authContext';
+
+export const AuthContext: React.Context<AuthContextType | null> =
+  createContext<AuthContextType | null>(null);
+
+export function AuthContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+}
