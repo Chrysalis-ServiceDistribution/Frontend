f9d8c10f967d71ac4d3439e789999ebfabb500da
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-05-18 13:57:42 -0400

    fix page wanting to stretch and look pretty

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { Theme } from '@radix-ui/themes';
+
 import Router from './pages/Router/Router';
 import { AuthContextProvider } from './contexts/AuthContext';
 
-import './reset.css';
-import '@radix-ui/themes/styles.css';
-import './index.css';
 
 function App() {
   return (
     <>
-      <Theme accentColor="mint" appearance="dark" className="background">
-        <AuthContextProvider>
-          <Router />
-        </AuthContextProvider>
-      </Theme>
+      <AuthContextProvider>
+        <Router />
+      </AuthContextProvider>
     </>
   );
 }
