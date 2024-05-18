e8d1a7085fcc8d0b811ec039c2c98f25b7cca3aa
Fekinox <williamowenfowlk@gmail.com>
2024-05-18 13:29:50 -0400

    Add radio field editing and deletion

--- src/components/formField/RadioFormField.tsx
+++ src/components/formField/RadioFormField.tsx
@@ -1,9 +1,62 @@
-import { Box } from '@radix-ui/themes';
+import { Box, Flex, TextField, Button } from '@radix-ui/themes';
 import { RadioField, FormFieldType } from './formField';
+import React from 'react';
+import { RadioGroup, RadioGroupIndicator } from '@radix-ui/react-radio-group';
+import NoOutlineIconButton from './NoOutlineIconButton';
+import { Cross1Icon, PlusIcon } from '@radix-ui/react-icons';
 
 export default function RadioFormField(props: {
   field: RadioField;
   onChange: (field: FormFieldType) => void | undefined;
 }) {
-  return <Box></Box>;
+  function editChoice(index: number) {
+    return (evt: React.ChangeEvent<HTMLInputElement>) => {
+      const newChoices = [...props.field.choices];
+      newChoices.splice(index, 1, evt.currentTarget.value);
+      const newField = new RadioField(props.field.prompt, newChoices);
+      props.onChange(newField);
+    };
+  }
+
+  function newChoice() {
+    const newChoices = [
+      ...props.field.choices,
+      `Choice ${props.field.choices.length + 1}`,
+    ];
+
+    const newField = new RadioField(props.field.prompt, newChoices);
+    props.onChange(newField);
+  }
+
+  function deleteChoice(index: number) {
+    return () => {
+      const newChoices = [...props.field.choices];
+      newChoices.splice(index, 1);
+      const newField = new RadioField(props.field.prompt, newChoices);
+      props.onChange(newField);
+    };
+  }
+
+  return (
+    <Flex direction="column">
+      {props.field.choices.map((choice, idx) => {
+        return (
+          <Flex gap="2">
+            <NoOutlineIconButton onClick={deleteChoice(idx)}>
+              <Cross1Icon />
+            </NoOutlineIconButton>
+            <TextField.Root
+              placeholder="Choice text..."
+              value={choice}
+              onChange={editChoice(idx)}
+            ></TextField.Root>
+          </Flex>
+        );
+      })}
+      <Button onClick={newChoice}>
+        <PlusIcon />
+        Add new choice
+      </Button>
+    </Flex>
+  );
 }
