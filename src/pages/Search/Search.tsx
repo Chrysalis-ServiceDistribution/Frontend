574113905754aa1a2e28aeff1dd5dc68e5f1e6f7
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-05-20 15:13:05 -0400

    add more

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,17 +1,32 @@
-import { Box, Flex } from '@radix-ui/themes';
+import { TextField, Flex } from '@radix-ui/themes';
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
+import { Service, Task } from '../../classes/service/service';
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Search() {
-
+  const [services, setServices] = useState([]);
   //get all services on pageload
   useEffect(() => {
-    console.log('search');
-  }
-  , []);
-  return(
-    <Flex>
-      <Box>Search</Box>
+    console.log('grabbing services on pageload');
+
+  }, []);
+
+  const handelSearchChange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log((e.target as HTMLInputElement).value);
+  };
+  return (
+    <Flex justify='center' align='center' direction='column'>
+      <TextField.Root
+        placeholder="Search the docs…"
+        onInput={(e) => handelSearchChange(e)}
+      >
+        <TextField.Slot>
+          <MagnifyingGlassIcon height="16" width="16" />
+        </TextField.Slot>
+      </TextField.Root>
+
+
     </Flex>
-  )
+  );
 }
