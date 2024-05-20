894e88e04999785a5739c20d3e201cb88f94fd71
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-05-20 16:32:59 -0400

    finish search

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,24 +1,25 @@
-import { TextField, Flex } from '@radix-ui/themes';
+import { TextField, Flex, ScrollArea } from '@radix-ui/themes';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
-import * as dummyData from '../UserServices/services.ts';
-import { Service, Task } from '../../classes/service/service';
+import dummyData from '../UserServices/services.ts';
+import { Service } from '../../classes/service/service';
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
 import { useState, useEffect } from 'react';
 
 export default function Search() {
-  const [services, setServices] = useState([]);
+  const [services, setServices] = useState<Service[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
   //get all services on pageload
   useEffect(() => {
-    console.log('grabbing services on pageload');
+    //TODO: fetch services from the server
     setServices(dummyData);
-
   }, []);
 
   const handelSearchChange = (e: React.FormEvent<HTMLInputElement>) => {
-    console.log((e.target as HTMLInputElement).value);
+    setSearchQuery((e.target as HTMLInputElement).value);
   };
+
   return (
-    <Flex justify='center' align='center' direction='column'>
+    <Flex justify="center" align="center" direction="column" gap="5">
       <TextField.Root
         placeholder="Search the docs…"
         onInput={(e) => handelSearchChange(e)}
@@ -27,8 +28,18 @@ export default function Search() {
           <MagnifyingGlassIcon height="16" width="16" />
         </TextField.Slot>
       </TextField.Root>
-
-
+      <ScrollArea style={{ width: '100%', height: '100%' }}>
+        <Flex direction="column" gap="6" justify="center">
+          {services
+            .filter((service) =>
+              service.name.toLowerCase().includes(searchQuery.toLowerCase()),
+            )
+            .map((service,index) => (
+              //TODO: change key when services are better typed
+              <ServiceCard key={index} service={service} />
+            ))}
+        </Flex>
+      </ScrollArea>
     </Flex>
   );
 }
